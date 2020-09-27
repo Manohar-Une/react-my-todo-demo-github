59f304e240cca22d92fa4793d04fe9f56e2c36d6
Manohar-Une <unemanohar17@gmail.com>
2020-09-27 13:29:44 +0530

    Updates

---
+++ precache-manifest.6f714b74cb14709ac09f33ea306cea6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83f790b990317e3fa46b71dce748ac66",
+    "url": "/react-my-todo-demo-github/index.html"
+  },
+  {
+    "revision": "09023f750e6deead2d9d",
+    "url": "/react-my-todo-demo-github/static/css/main.9a15704f.chunk.css"
+  },
+  {
+    "revision": "6911339b2fde8700e68a",
+    "url": "/react-my-todo-demo-github/static/js/2.83410193.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-my-todo-demo-github/static/js/2.83410193.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09023f750e6deead2d9d",
+    "url": "/react-my-todo-demo-github/static/js/main.e5e0c497.chunk.js"
+  },
+  {
+    "revision": "39920c8f69f59dd1b2e2",
+    "url": "/react-my-todo-demo-github/static/js/runtime-main.b7eb906d.js"
+  }
+]);
